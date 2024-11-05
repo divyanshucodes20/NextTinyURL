6537cc117c22a9d957a4cbc1ff8af282c68a2eb4
divyanshucodes20 <155738552+divyanshucodes20@users.noreply.github.com>
2024-11-05 17:40:21 +0530

    Implement dynamic page for URL redirection based on short URL parameter

---
+++ src/app/[shortUrl]/page.js
@@ -0,0 +1,16 @@
+import { redirect } from "next/navigation"
+import clientPromise from "../../../lib/mongodb"
+export default async function Page({params}){
+    const shortUrl=(await params).shortUrl
+    const client=await clientPromise;
+    const db=client.db("nexttinyurl")
+    const collection=db.collection("url")
+    const doc=await collection.findOne({shortUrl:shortUrl})
+    if(doc){
+      redirect(doc.url)
+    }
+    else{
+        redirect(`${NEXT_PUBLIC_HOST}`)
+    }
+    return <div>My Post:{url}</div>
+}
